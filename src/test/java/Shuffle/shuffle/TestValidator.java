f71e3c276e03ba004a2c2b5447869071a3e9e01d
Ryotaro Ikeda <ryo@longsleeper.com>
2019-01-19 21:23:32 +0900

    Add message check on Validator class

--- src/test/java/Shuffle/shuffle/TestValidator.java
+++ src/test/java/Shuffle/shuffle/TestValidator.java
@@ -3,7 +3,9 @@ package Shuffle.shuffle;
 import java.util.List;
 
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.*;
@@ -13,35 +15,42 @@ public class TestValidator {
     final static private long MAX_INPUT_SIZE = 1;
     final private Validator sut = new Validator(MAX_INPUT_SIZE);
 
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
     /**
      * 引数がnullのとき
      */
-    @Test(expected = IllegalArgumentException.class)
+    @Test
     public void inputIsNull() {
+        thrown.expectMessage(Validator.ERROR_INPUT_NULL);
         sut.validate(null);
     }
 
     /**
      * 引数に半角英数字以外の文字が含まれるとき
      */
-    @Test(expected = IllegalArgumentException.class)
+    @Test
     public void inputIsIllegalCharacters() {
+        thrown.expectMessage(Validator.ERROR_ILLEGAL_CHARACTERS);
         sut.validate("~");
     }
 
     /**
      * 引数が空文字のとき
      */
-    @Test(expected = IllegalArgumentException.class)
+    @Test
     public void inputIsEmptyString() {
+        thrown.expectMessage(Validator.ERROR_EMPTY_STRING);
         sut.validate("");
     }
 
     /**
      * 引数が入力最大文字数を超えるとき
      */
-    @Test(expected = IllegalArgumentException.class)
+    @Test
     public void inputExceedsMaxInputSize() {
+        thrown.expectMessage(Validator.ERROR_INPUT_SIZE_OVER + MAX_INPUT_SIZE);
         sut.validate("aa");
     }
 }
